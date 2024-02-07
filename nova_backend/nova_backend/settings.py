10a4dfa8d5240cf43bccb3662872642d9b6996c5
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-02-07 13:16:56 +0200

    another finall commit

--- nova_backend/nova_backend/settings.py
+++ nova_backend/nova_backend/settings.py
@@ -26,7 +26,7 @@ CSRF_COOKIE_SECURE = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://nova-169j.onrender.com"]
 
 
 # Application definition
