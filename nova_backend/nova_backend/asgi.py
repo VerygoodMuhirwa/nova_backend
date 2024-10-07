8ef92236bfdf19b376c5eaca31c579ffecb4568f
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-10-07 06:06:48 +0200

    Solving dependency conflicts

--- nova_backend/nova_backend/asgi.py
+++ nova_backend/nova_backend/asgi.py
@@ -1,16 +1,17 @@
 import os
 import django
+
+# Set the Django settings module before any Django-related imports
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nova_backend.settings')
+
+# Setup Django before importing other Django components
 django.setup()
+
+# Now import Django and Channels modules
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from sensorapp.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nova_backend.settings')
-
-# Setup Django
-
 
 # Set up ASGI application
 application = ProtocolTypeRouter({
